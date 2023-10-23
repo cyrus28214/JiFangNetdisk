b3824747ca16e89732ca992a01524135507dd8e0
cyrus28214 <cyrus28214@gmail.com>
2023-10-23 23:34:50 +0800

    create start.sh; use jinja2 template extension

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 app = Flask(__name__)
 directory_path = './static/files/'
+template_vars = ('os', 'directory_path')
 
-for i in ('os', 'directory_path'):
+for i in template_vars:
     app.jinja_env.globals[i] = eval(i)
 
-@app.route('/s/', methods = ['GET', 'POST'])
-@app.route('/s/<path:path>/', methods = ['GET', 'POST'])
-def browseDir(path = ''):
-    local_path = directory_path + path
-    if(request.method == 'POST' and os.path.isdir(local_path)):
+@app.route('/', methods = ['GET'])
+@app.route('/<path:path>', methods = ['GET'])
+def browse(path = ''):
+    print(path)
+    local_path = os.path.join(directory_path, path)
+    if os.path.isdir(local_path):
+        return render_template('browse.j2', path=path)
+    if os.path.isfile(local_path):
+        return send_from_directory('', local_path)
+    return redirect('/')
+
+@app.route('/', methods = ['POST'])
+@app.route('/<path:path>', methods = ['POST'])
+def upload(path = ''):
+    failed_list = []
+    local_path = os.path.join(directory_path, path)
+    if(os.path.isdir(local_path)):
         files = request.files.getlist('uploaded_files')
         for f in files:
             if(f):
                 f.save(os.path.join(local_path, f.filename))
-    if(os.path.isdir(local_path)):
-        return render_template('browseDir.html', path=path)
-    return redirect('/s/')
+    return render_template('uploadResponse.j2', redirect_url='/'+path, message='文件上传成功')
 
-@app.route('/')
-def Index():
-    return redirect('/s/')
 
 if(__name__ == '__main__'):
-    app.run()
+    app.run(debug=True)
         
--- start.bat
+++
@@ -1,4 +0,0 @@
-call ..\venv\Scripts\activate
-set FLASK_ENV=development
-call flask run
-pause
